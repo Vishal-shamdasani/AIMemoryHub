a9563ce33e16bef690f03285b8a7571303a6284d
Vishal-shamdasani <shamdasani46@gmail.com>
2025-07-31 20:09:02 +0530

    requirements

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Welcome to the AIMemoryHub! Use /download-json to download the JSON file."
+
 @app.route('/download-json', methods=['GET'])
 def download_json():
     return send_file('data.json', mimetype='application/json', as_attachment=True)
